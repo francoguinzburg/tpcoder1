acf67c6edf05f46283284ecbce7f55a42230089d
Franco  Guinzburg <fguinzburg0@gmail.com>
2023-02-22 21:38:33 -0300

    ahi ta

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+  return (
+    <div>
+        <p>Hola {props.nombre} {props.apellido}!</p>
+    </div>
+  )
+}
+
+export default ItemListContainer
